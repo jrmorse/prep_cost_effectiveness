7617c315e2bc11b9e56db2d7df9f8273f4161681
jrmorse <morse.john@gmail.com>
2020-04-02 21:22:51 -0400

    shiny problems

---
+++ shiny/app.R
@@ -0,0 +1,33 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+
+    sliderInput("n", "Select Graphs", 1,5,1),
+    plotOutput("plot")
+)
+
+
+server <- function(input, output, session) {
+    output$plot <- renderImage({
+        # When input$n is 1, filename is ./shiny/image1.jpeg
+        filename <- normalizePath(file.path('./shiny',
+                                            paste('image', input$n, '.jpeg', sep='')))
+        
+        # Return a list containing the filename
+        list(src = filename,
+             width = 700,
+             height = 500)
+    }, deleteFile = FALSE)
+}
+shinyApp(ui, server)
+
