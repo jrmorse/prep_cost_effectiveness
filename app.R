0b9b869fc720f3d2d72ca84b3896fbf2eb81b5cd
jrmorse <morse.john@gmail.com>
2020-04-23 18:16:30 -0400

    Third panel

--- app.R
+++ app.R
@@ -64,7 +64,11 @@ ui <- fluidPage(
                                                    financial resources must be directed towards treatment. In my study, I aim to
                                                    link the advent of PrEP in the United States to this increased observation of
                                                    STIs, before discerning the cost efficiency of PrEP and its’ impact on the
-                                                   United States’ healthcare system."),
+                                                   United States’ healthcare system."), br(),
+                                                   
+                                                   # Here I load a graph to show the decrease in HIV diagnosis.
+                                                  
+                                                   plotOutput("hivTotal", width = "100%", height = "100%"), br(),
                                                    
                                                    p("With this project, I have aggregrated data from diverse sources including 
                                                    the CDC, Rollins School of Public Health with Gilead Sciences, and academic
@@ -85,14 +89,17 @@ ui <- fluidPage(
                                                    tabPanel("HIV",
                                                             sidebarLayout(
                                                               sidebarPanel(
-                                                                
                                                                 # Set an icon for the webpage along with wording
                                                                 
                                                                 HTML('<script> document.title = "HIV in the United States & The Promise of PrEP"; </script>'),
                                                                 tags$head(tags$link(rel="shortcut icon", href="https://upload.wikimedia.org/wikipedia/commons/e/e6/World_Aids_Day_Ribbon.png")),
                                                                 
                                                                 # Add in short description about what this plot is.
-                                                                
+                                                                selectInput("sexInput", "Sex", c(
+                                                                    "Male",
+                                                                    "Female"), multiple = TRUE)
+                                                              ,
+                                                              
                                                                 p(tags$em("Select a transmission category to view nation totals on a yearly basis")),
                                                                 
                                                                 # Include the option for users to select what transmission category they would like to see.
@@ -101,17 +108,37 @@ ui <- fluidPage(
                                                                                                                                "Injection drug use",
                                                                                                                                "Male-to-male sexual contact",
                                                                                                                                "Male-to-male sexual contact and injection drug use",
-                                                                                                                               "Other")
-                                                                )
-                                                              ),
+                                                                                                                               "Other"), multiple = TRUE)
+                                                                ),
                                                               mainPanel(
-                                                                
-                                                                # Load in the plot.
-                                                                
-                                                                plotOutput("plot")
-                                                              )
-                                                            )
+                                                                # Here I add in a fluid row. I'm not sure if I'll change this so that it matches the aesthetic of the first page.
+                                                                  
+                                                                  fluidRow(column(2), column(8,
+                                                                                             # Here I load in the plot
+                                                                                             plotOutput("plot")
+                                                                                             )
+                                                                           )
+                                                                  )
+                                                              ),
+                                                            
+                                                            # Here I add the second chart to this page.
+                                                            
+                                                                sidebarPanel(
+                                                                    selectInput("raceInput", "Race", c("Black/African American",
+                                                                                                       "White",
+                                                                                                       "Hispanic/Latino",
+                                                                                                       "Asian"), multiple = TRUE)
+                                                                                ),
+                                                                    mainPanel(
+                                                                        fluidRow(column(2), column(8, 
+                                                                        plotOutput("race")
+                                                                    )
+                                                                    
+                                                                )
                                                    )
+               ),
+               tabPanel("PrEP"),
+               imageOutput("medicine2", width = "100%", height = "100%")
     )
 )
 
@@ -138,18 +165,70 @@ server <- function(input, output, session) {
     }, deleteFile = FALSE
     )
     
+    output$hivTotal <- renderPlot({
+        hiv_aids_year %>% 
+        ggplot(aes(x = year, y = total_cases)) +
+            geom_point() +
+            geom_text(aes(x = year, y = total_cases, label = total_cases),vjust = -1, nudge_y = .5) +
+            labs(title="HIV Diagnoses by Year", subtitle = "2007-2017") +
+            theme_classic() +
+            scale_x_continuous(
+                name = "Year",
+                breaks = seq(2007,2017,1),
+                label = c("2007", "2008", "2009", "2010", "2011","2012","2013","2014","2015","2016","2017")) +
+            scale_y_continuous(
+                name = "New Diagnoses",
+                limits=c(35000, 50000))
+    },
+    
+    height = 400,
+    width = 700
+    )
+    
     # Create a plot of nationwide users
      output$plot <- renderPlot({
        filtered <-
          hiv_aids_transmission %>% 
-         filter(transmission_category == input$transmissionInput)
+         filter(transmission_category %in% input$transmissionInput,
+                sex %in% input$sexInput)
        
        ggplot(data = filtered, aes(x = year, y = total_cases)) +
          geom_col() +
-         theme_classic()
-       
+         theme_classic() +
+         labs(title="HIV Diagnosis by Sex and Category", subtitle = "2007-2017") +
+           scale_x_continuous(
+               name = "Year",
+               breaks = seq(2007,2017,1),
+               label = c("2007", "2008", "2009", "2010", "2011", "2012", "2013","2014", "2015", "2016"," 2017")) +
+           scale_y_continuous(
+               name = "New Diagnoses")
+      
      }) 
-
+     # Create plot of HIV diagnosis by race
+     output$race <- renderPlot({
+         filtered2<-
+             hiv_aids_maletomalesexualcontact %>% 
+             filter(race_ethnicity %in% input$raceInput)
+         ggplot(filtered2, aes(x = year, y = total_cases, fill = race_ethnicity)) +
+             geom_col(position = "dodge") +
+            # facet_wrap(~ race_ethnicity) +
+             labs(title="HIV Diagnosis Male-to-male Sexual Contact", subtitle = "2007-2017") +
+             theme_classic() +
+             scale_x_continuous(
+                 name = "Year",
+                 breaks = seq(2007,2017,2),
+                 label = c("2007", "2009", "2011", "2013", "2015", "2017")) +
+             scale_y_continuous(
+                 name = "New Diagnoses"
+             )
+     })
+     output$medicine2 <- renderImage({
+         # Return a list containing the filename and alt text
+         list(src = './graphics/medicine2.png',
+              height = 450,
+              width = 800, style="display: block; margin-left: auto; margin-right: auto;")
+     }, deleteFile = FALSE
+     )
 }
 
 shinyApp(ui = ui, server = server)
